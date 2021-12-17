7ade966067fb8d799e7cc67b15e29d6a32231a05
luckykavya55 <luckykavya735@gmail.com>
2021-12-17 15:37:27 +0530

    updating the db java

--- dbutils.java
+++ dbutils.java
@@ -1,10 +1,6 @@
-this file contains he inf about java
-=======
+this file contains he inf about 
 hello guys git command is very imp....
 dont miss the classes
   this line is added to the fil
   i am adding this linee
-addingg neew line in remote repo
->>>>>>> c21314fa6a64fe0bbcce6512768f461c62029343
-=======
->>>>>>> parent of 489159b (updated dbutils.sh fr cher pic)
+addingg neew line in remote 
